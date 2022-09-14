9a24d4d2a4b6c865bdb88ab11f5018f1db41701a
Jesse Houwing <jesse.houwing@gmail.com>
2022-09-14 08:32:53 +0000

    Removing classic valet scripts

--- valet/plugin/DotNetCoreCLI.rb
+++ valet/plugin/DotNetCoreCLI.rb
@@ -1,35 +1,28 @@
 transform "DotNetCoreCLI@2" do |item|
-    projects = item["projects"]
-    command = item["command"]
-  
-    if(command.nil?)
-      command = "build"
-    end
-  
-    if (item["arguments"] != nil)
-      item['arguments'] = item['arguments'].gsub("BuildConfiguration", "BUILDCONFIGURATION")
-    end
-  
-    {
-      shell: "bash",
-      run: "shopt -s globstar; for f in ./**/*.csproj; do dotnet #{command} $f #{item['arguments'] } ; done"
-    }
-   end
+    
+  projects = item["projects"]
+  command = item["command"]
+
+  if(command.nil?)
+    command = "build"
+  end
+
+  {
+    shell: "bash",
+    run: "shopt -s globstar; for f in ./**/*.csproj; do dotnet #{command} $f #{item['arguments'] } ; done"
+  }
+end
 
 transform "5541a522-603c-47ad-91fc-a4b1d163081b@2" do |item|
-    projects = item["projects"]
-    command = item["command"]
-  
-    if(command.nil?)
-      command = "build"
-    end
-  
-    if (item["arguments"] != nil)
-      item['arguments'] = item['arguments'].gsub("BuildConfiguration", "BUILDCONFIGURATION")
-    end
-  
-    {
-      shell: "bash",
-      run: "shopt -s globstar; for f in ./**/*.csproj; do dotnet #{command} $f #{item['arguments'] } ; done"
-    }
+   projects = item["projects"]
+  command = item["command"]
+
+  if(command.nil?)
+    command = "build"
+  end
+
+  {
+    shell: "bash",
+    run: "shopt -s globstar; for f in ./**/*.csproj; do dotnet #{command} $f #{item['arguments'] } ; done"
+  }
 end
