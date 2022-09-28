f9b6e016505a7818502be90edaf76afb809bdf31
Jesse Houwing <jesse.houwing@gmail.com>
2022-09-28 17:41:16 +0200

    Update DotNetCoreCLI.rb

--- valet/plugin/DotNetCoreCLI.rb
+++ valet/plugin/DotNetCoreCLI.rb
@@ -1,20 +1,6 @@
-transform "DotNetCoreCLI@2" do |item|
+transform "DotNetCoreCLI@2", "5541a522-603c-47ad-91fc-a4b1d163081b@2" do |item|
     
   projects = item["projects"]
-  command = item["command"]
-
-  if(command.nil?)
-    command = "build"
-  end
-
-  {
-    shell: "bash",
-    run: "shopt -s globstar; for f in ./**/*.csproj; do dotnet #{command} $f #{item['arguments'] } ; done"
-  }
-end
-
-transform "5541a522-603c-47ad-91fc-a4b1d163081b@2" do |item|
-   projects = item["projects"]
   command = item["command"]
 
   if(command.nil?)
